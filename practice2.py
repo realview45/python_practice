bcec8a6f26228ec86d77de8c78b4cc37acfc5532
realview45 <kjink0125@gmail.com>
2026-04-05 18:20:11 +0900

    6.숫자처리함수 abs, pow, max, min math임포트해서 floor, ceil, sqrt

--- practice2.py
+++ practice2.py
@@ -30,3 +30,12 @@ print((3>0) | (3>5)) #True
 print(5 > 4 > 3) #True
 print(5 > 4 > 7) #False
 
+print(abs(-5))#5
+print(pow(4,2))#4^2 = 4*4 = 16
+print(max(5,12))#12
+print(min(5,12))#5
+
+from math import *
+print(floor(4.99))#내림 4
+print(ceil(3.14))#올림 4
+print(sqrt(16))#제곱근 4.0
