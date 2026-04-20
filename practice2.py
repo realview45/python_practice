58d8033dbbe1a9b84900b532c16b0d676e9d06f1
realview45 <kjink0125@gmail.com>
2026-04-20 22:33:50 +0900

    9.문자열포맷

--- practice2.py
+++ practice2.py
@@ -67,3 +67,25 @@ sentence3="""
 파이썬은 쉬워요
 """
 print(sentence3)
+
+#방법1
+print("나는 %d살입니다." % 20)
+print("나는 %s를 좋아해요." % "파이썬")
+print("Apple은 %c로 시작해요." % "A")
+# %s
+print("나는 %s살입니다." %20)
+print("나는 %s색과 %s색을 좋아해요" %("파란","빨간"))
+
+#방법2
+print("나는 {}살입니다.".format(20))
+print("나는 {}색과 {}색을 좋아해요.".format("파란","빨간"))
+print("나는 {0}색과 {1}색을 좋아해요.".format("파란","빨간"))
+print("나는 {1}색과 {0}색을 좋아해요.".format("파란","빨간"))
+
+#방법3
+print("나는 {age}살이며, {color}색을 좋아해요.".format(age=20,color="빨간"))
+
+#방법4(v3.6이상~)
+age = 20
+color = "빨간"
+print("나는 {age}살이며, {color}색을 좋아해요")
