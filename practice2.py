508dac9eb8e6d88c67ab53b14dd2ec9a588d3db9
realview45 <kjink0125@gmail.com>
2026-04-20 22:40:12 +0900

    10.탈출문자

--- practice2.py
+++ practice2.py
@@ -89,3 +89,24 @@ print("나는 {age}살이며, {color}색을 좋아해요.".format(age=20,color="
 age = 20
 color = "빨간"
 print("나는 {age}살이며, {color}색을 좋아해요")
+
+#탈출문자
+print("백문이 불여일견\n백견이 불여일타")
+
+#저는 "나도코딩"입니다
+print("저는 '나도코딩'입니다.")
+print('저는 "나도코딩"입니다.')
+print("저는 \"나도코딩\"입니다.")
+print("저는 \'나도코딩\'입니다.")
+
+#\\ : 문장 내에서 \
+print("C:\\Users\\Playdata\\Downloads\\python\\python_practice")
+
+#\r : 커서를 맨앞으로 이동
+print("Red Apple\rPine")
+
+#\b : 백스페이스 (한글자삭제)
+print("Redd\bApple")
+
+#\t : 탭
+print("Red\tApple")
